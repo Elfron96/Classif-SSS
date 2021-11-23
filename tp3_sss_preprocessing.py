80520f4766889237f1872309aafcd8878f8f60d3
Elfron96 <b.lecoeuvre@gmail.com>
2021-11-23 23:17:37 +0100

    coucou

--- tp3_sss_preprocessing.py
+++ tp3_sss_preprocessing.py
@@ -19,7 +19,7 @@ from kneed import *
 from selectClassArea import selectClassArea
 plt.ioff()
 
-image_plot = 1
+image_plot = 0
 
 #%% Partie 1
 
@@ -30,154 +30,154 @@ data = loadmat("12_data.mat")
 # Affichage de l'image
 data_img = data['img']
 
-if image_plot:
-    plt.figure()
-    plt.title("Affichage de l'image'")
-    plt.imshow(data_img, aspect = "auto")
-    plt.xlabel("Echantillon")
-    plt.ylabel("N° de Pings")
-    plt.colorbar(label ="Backscatter [dB]")
+# if image_plot:
+#     plt.figure()
+#     plt.title("Affichage de l'image'")
+#     plt.imshow(data_img, aspect = "auto")
+#     plt.xlabel("Echantillon")
+#     plt.ylabel("N° de Pings")
+#     plt.colorbar(label ="Backscatter [dB]")
 
 
 
 
-#%% classif non_supervisée
+# #%% classif non_supervisée
 
-N = data_img.reshape((data_img.size, 1))# Size prend le nombre d'éléments dans la matrice (à ne pas confondre avec shape)
-Nech = N[::50]
+# N = data_img.reshape((data_img.size, 1))# Size prend le nombre d'éléments dans la matrice (à ne pas confondre avec shape)
+# Nech = N[::50]
 
-## Test sur un sous échantillon 
-# Application du K-Means : 
-model_kmeans = KMeans(n_clusters = 3, n_init=20)
-model_kmeans.fit(Nech)
+# ## Test sur un sous échantillon 
+# # Application du K-Means : 
+# model_kmeans = KMeans(n_clusters = 3, n_init=20)
+# model_kmeans.fit(Nech)
 
-kmeans_kwargs = {
-"init": "random",
-"n_init": 20,
-}
+# kmeans_kwargs = {
+# "init": "random",
+# "n_init": 20,
+# }
 
-# # A list holds the SSE values for each k
-sse = []
-for k in range(1, 11):
-    kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
-    kmeans.fit(Nech)
-    sse.append(kmeans.inertia_)
+# # # A list holds the SSE values for each k
+# sse = []
+# for k in range(1, 11):
+#     kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+#     kmeans.fit(Nech)
+#     sse.append(kmeans.inertia_)
 
-if image_plot:
-    plt.figure()
-    plt.style.use("fivethirtyeight")
-    plt.plot(range(1, 11), sse)
-    plt.xticks(range(1, 11))
-    plt.xlabel("Number of Clusters")
-    plt.ylabel("SSE")
-    kl = KneeLocator(range(1, 11), sse, curve="convex", direction="decreasing")
-    print(kl.elbow)
-
-
-
-
-
-# #Prédictions des classes : 
-Yech = model_kmeans.predict(Nech)
-
-# # Affichage des résultats : 
-if image_plot:
-    plt.figure()
-    plt.scatter(Nech, Yech, c= Yech, label = "Classes")
-    plt.title("Classes déterminées par K-Means")
-    plt.xlabel("Pixels")
-    plt.ylabel("Classes")
-
-# ## K-Means sur toute l'image : 
-# # On garde l'a priori avec 3 classes : 
-model_kmeans.fit(N)
-Y = model_kmeans.predict(N)
-
-if image_plot:
-    plt.figure()
-    label_plot, plot =  displayImageLabel(Y, data_img)
-
-    plt.plot(Y,label=label_plot)
+# if image_plot:
+#     plt.figure()
+#     plt.style.use("fivethirtyeight")
+#     plt.plot(range(1, 11), sse)
+#     plt.xticks(range(1, 11))
+#     plt.xlabel("Number of Clusters")
+#     plt.ylabel("SSE")
+#     kl = KneeLocator(range(1, 11), sse, curve="convex", direction="decreasing")
+#     print(kl.elbow)
 
 
 
 
-#%% On enleve colonne d'eau et classification
+
+# # #Prédictions des classes : 
+# Yech = model_kmeans.predict(Nech)
+
+# # # Affichage des résultats : 
+# if image_plot:
+#     plt.figure()
+#     plt.scatter(Nech, Yech, c= Yech, label = "Classes")
+#     plt.title("Classes déterminées par K-Means")
+#     plt.xlabel("Pixels")
+#     plt.ylabel("Classes")
+
+# # ## K-Means sur toute l'image : 
+# # # On garde l'a priori avec 3 classes : 
+# model_kmeans.fit(N)
+# Y = model_kmeans.predict(N)
+
+# if image_plot:
+#     plt.figure()
+#     label_plot, plot =  displayImageLabel(Y, data_img)
+
+#     plt.plot(Y,label=label_plot)
+
+
+
+
+# #%% On enleve colonne d'eau et classification
 
 data_img = data_img[:,320:]
-# print(data_img.shape)
-N = data_img.reshape((data_img.size, 1))# Size prend le nombre d'éléments dans la matrice (à ne pas confondre avec shape)
-Nech = N[::50]
-Yech = model_kmeans.predict(Nech)
-model_kmeans = KMeans(n_clusters = 3, n_init=20)
-model_kmeans.fit(Nech)
+# # print(data_img.shape)
+# N = data_img.reshape((data_img.size, 1))# Size prend le nombre d'éléments dans la matrice (à ne pas confondre avec shape)
+# Nech = N[::50]
+# Yech = model_kmeans.predict(Nech)
+# model_kmeans = KMeans(n_clusters = 3, n_init=20)
+# model_kmeans.fit(Nech)
 
 
-if image_plot:
-    plt.figure()
-    plt.scatter(Nech, Yech, c= Yech, label = "Classes")
-    plt.title("Classes déterminées par K-Means")
-    plt.xlabel("Pixels")
-    plt.ylabel("Classes")
+# if image_plot:
+#     plt.figure()
+#     plt.scatter(Nech, Yech, c= Yech, label = "Classes")
+#     plt.title("Classes déterminées par K-Means")
+#     plt.xlabel("Pixels")
+#     plt.ylabel("Classes")
 
-# ## K-Means sur toute l'image : 
-# # On garde l'a priori avec 3 classes : 
-model_kmeans.fit(N)
-Y = model_kmeans.predict(N)
+# # ## K-Means sur toute l'image : 
+# # # On garde l'a priori avec 3 classes : 
+# model_kmeans.fit(N)
+# Y = model_kmeans.predict(N)
 
-if image_plot:
-    plt.figure()
-    label_plot, plot =  displayImageLabel(Y, data_img)
-#%% Correction de l'image par moyennage simple et affichage
+# if image_plot:
+#     plt.figure()
+#     label_plot, plot =  displayImageLabel(Y, data_img)
+# #%% Correction de l'image par moyennage simple et affichage
 
 moy = np.mean(data_img, axis = 0)
 
-# correction
+# # correction
 img_corrige_moy = data_img / moy
-if image_plot:
-    plt.figure()
+# if image_plot:
+#     plt.figure()
 
-    plt.title("Affichage de l'image initiale")
-    plt.imshow(data_img, aspect = "auto")
-    plt.xlabel("Échantillon")
-    plt.ylabel("Pings")
-    plt.colorbar(label ="BS [dB]")
+#     plt.title("Affichage de l'image initiale")
+#     plt.imshow(data_img, aspect = "auto")
+#     plt.xlabel("Échantillon")
+#     plt.ylabel("Pings")
+#     plt.colorbar(label ="BS [dB]")
 
-    plt.figure()
-    plt.plot(moy)
-    plt.xlabel("Échantillon")
-    plt.ylabel("Moyenne sur l'ensemble des pings")
-    plt.title("Evolution de la moyenne sur l'ensemble des pings en fonction du numéro d'échantillon")
+#     plt.figure()
+#     plt.plot(moy)
+#     plt.xlabel("Échantillon")
+#     plt.ylabel("Moyenne sur l'ensemble des pings")
+#     plt.title("Evolution de la moyenne sur l'ensemble des pings en fonction du numéro d'échantillon")
 
-    plt.figure()
-    plt.imshow(img_corrige_moy)
-    plt.title("Image corrigée de la moyenne")
-    plt.xlabel("Échantillon")
-    plt.ylabel("Moyenne sur l'ensemble des pings")
-    plt.colorbar(label =  "Backscatter [dB]")
+#     plt.figure()
+#     plt.imshow(img_corrige_moy)
+#     plt.title("Image corrigée de la moyenne")
+#     plt.xlabel("Échantillon")
+#     plt.ylabel("Moyenne sur l'ensemble des pings")
+#     plt.colorbar(label =  "Backscatter [dB]")
 
-    plt.figure()
-    plt.plot(np.mean(img_corrige_moy, axis = 0))
-    plt.xlabel("Numéro d'échantillon")
-    plt.ylabel("Moyenne sur l'ensemble des pings")
-    plt.title("Evolution de la moyenne sur l'ensemble des pings en fonction du numéro d'échantillon sur l'image corrigé")
+#     plt.figure()
+#     plt.plot(np.mean(img_corrige_moy, axis = 0))
+#     plt.xlabel("Numéro d'échantillon")
+#     plt.ylabel("Moyenne sur l'ensemble des pings")
+#     plt.title("Evolution de la moyenne sur l'ensemble des pings en fonction du numéro d'échantillon sur l'image corrigé")
 
 
-# classification
+# # classification
     
-N_corr_moyenne = img_corrige_moy.reshape((img_corrige_moy.size, 1))
+# N_corr_moyenne = img_corrige_moy.reshape((img_corrige_moy.size, 1))
 
-model_kmeans.fit(N_corr_moyenne)
-Y_corr_moy = model_kmeans.predict(N_corr_moyenne)
+# model_kmeans.fit(N_corr_moyenne)
+# Y_corr_moy = model_kmeans.predict(N_corr_moyenne)
 
-if image_plot:
-    plt.figure()
-    label_plot, plot =  displayImageLabel(Y_corr_moy,img_corrige_moy)
+# if image_plot:
+#     plt.figure()
+#     label_plot, plot =  displayImageLabel(Y_corr_moy,img_corrige_moy)
 
-    plt.show()
+#     plt.show()
 
 
-#%% Classification supervisée
+# # %% Classification supervisée
 
 # # imgClassNum,roi = selectClassArea(img_corrige_moy)
 
@@ -239,32 +239,39 @@ if image_plot:
 
 # plt.show()
 
-# #%% Calcul des descripteurs stats et calcul des features
-# feat, patchParams = cptPatchesStats(img_corrige_moy,[128,128],[32,32])
+#%% Calcul des descripteurs stats et calcul des features
+patchSize, patchShift = 128, 32
+plt.figure()
+for i in range(1,5):
+
+    feat, patchParams = cptPatchesStats(img_corrige_moy,[patchSize*(i*2),patchSize*(i*2)],[patchShift*(i/4),patchShift*(i/4)])
 
 
 
 
 
-# # classification par kmeans
 
+    # classification par kmeans
+
+        
+    N = feat[:,0:3]# Size prend le nombre d'éléments dans la matrice (à ne pas confondre avec shape)
+    # Nech = N[::50]
+    # print(N.shape)
+    ## Test sur un sous échantillon 
+    # Application du K-Means : 
+    model_kmeans = KMeans(n_clusters = 4, n_init=20)
+    model_kmeans.fit(N)
+    print(model_kmeans.labels_.shape)
+
+
+
+    # calcul de la position des centres
     
-# N = feat[:,0:3]# Size prend le nombre d'éléments dans la matrice (à ne pas confondre avec shape)
-# # Nech = N[::50]
-# # print(N.shape)
-# ## Test sur un sous échantillon 
-# # Application du K-Means : 
-# model_kmeans = KMeans(n_clusters = 4, n_init=20)
-# model_kmeans.fit(N)
-# print(model_kmeans.labels_.shape)
-
-
-
-# # calcul de la position des centres
-# plt.figure()
-# plt.imshow(img_corrige_moy)
-# y = np.mean(patchParams[:,0:2],axis=1)
-# x = np.mean(patchParams[:,2:4],axis=1)
-# # affichage
-# plt.scatter(x,y,s=20,c=model_kmeans.labels_)
-# plt.show()
+    plt.subplot(2,2,i)
+    plt.imshow(img_corrige_moy)
+    y = np.mean(patchParams[:,0:2],axis=1)
+    x = np.mean(patchParams[:,2:4],axis=1)
+    # affichage
+    plt.scatter(x,y,s=20,c=model_kmeans.labels_)
+    plt.title("Classification non supervisée, patchSize ="+str(patchSize*(i*2))+" et patchShift ="+str(patchShift*(i*2)))
+plt.show()
